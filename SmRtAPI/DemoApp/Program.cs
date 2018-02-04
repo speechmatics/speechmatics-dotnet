679eb8080a36329f7f88c2ee772493e8d4953799
James Gilmore <jamesg@speechmatics.com>
2018-02-04 15:53:28 +0000

    Comments in demo app

--- SmRtAPI/DemoApp/Program.cs
+++ SmRtAPI/DemoApp/Program.cs
@@ -8,24 +8,29 @@ namespace DemoApp
 {
     class Program
     {
+        private const string SampleAudio = "2013-8-british-soccer-football-commentary-alex-warner.mp3";
+
         // ReSharper disable once UnusedParameter.Local
         static void Main(string[] args)
         {
             var builder = new StringBuilder();
-            // wss://api.rt.speechmatics.io:9000/
-            using (var stream = File.Open("2013-8-british-soccer-football-commentary-alex-warner.mp3", FileMode.Open,
-                FileAccess.Read))
+
+            using (var stream = File.Open(SampleAudio, FileMode.Open, FileAccess.Read))
             {
                 try
                 {
+                    /*
+                     * The API constructor is passed the websockets URL, callbacks for the messages it might receive,
+                     * the language to transcribe (as a .NET CultureInfo object) and stream to read data from.
+                     */
                     var api = new SmRtApi("wss://api.rt.speechmatics.io:9000/",
                         s => builder.Append(s),
                         CultureInfo.GetCultureInfo("en-US"),
                         stream
-                        );
+                    );
+                    // Run() will block until the transcription is complete.
                     api.Run();
                     Console.WriteLine(builder.ToString());
-
                 }
                 catch (AggregateException e)
                 {
