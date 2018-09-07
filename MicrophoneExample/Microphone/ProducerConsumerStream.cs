42ec1b7e4e644de970d7ef5054f3e9d5d2e228d4
James Gilmore <jamesg@speechmatics.com>
2018-09-07 16:41:39 +0100

    Example using a microphone provided by NAudio.

---
+++ MicrophoneExample/Microphone/ProducerConsumerStream.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+
+namespace Speechmatics.Realtime.Microphone
+{
+    public class ProducerConsumerStream : Stream
+    {
+        private readonly MemoryStream _innerStream;
+        private long _readPosition;
+        private long _writePosition;
+
+        public ProducerConsumerStream()
+        {
+            _innerStream = new MemoryStream();
+        }
+
+        public override bool CanRead => true;
+        public override bool CanSeek => false;
+        public override bool CanWrite => true;
+
+        public override void Flush()
+        {
+            lock (_innerStream)
+            {
+                _innerStream.Flush();
+            }
+        }
+
+        public override long Length
+        {
+            get
+            {
+                lock (_innerStream)
+                {
+                    return _innerStream.Length;
+                }
+            }
+        }
+
+        public override long Position
+        {
+            get => throw new NotSupportedException();
+            set => throw new NotSupportedException();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            lock (_innerStream)
+            {
+                _innerStream.Position = _readPosition;
+                var red = _innerStream.Read(buffer, offset, count);
+                _readPosition = _innerStream.Position;
+
+                if (red == 0)
+                {
+                    // The buffer is empty but let's pretend it's not and send silence
+                    for (var i = 0; i < count; i++)
+                    {
+                        buffer[offset + i] = 0;
+                    }
+                    return count;
+                }
+
+                return red;
+            }
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            lock (_innerStream)
+            {
+                _innerStream.Position = _writePosition;
+                _innerStream.Write(buffer, offset, count);
+                _writePosition = _innerStream.Position;
+            }
+        }
+    }
+}
---
+++ MicrophoneExample/Microphone/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading.Tasks;
+using NAudio.Wave;
+using Newtonsoft.Json;
+using Speechmatics.Realtime.Client;
+using Speechmatics.Realtime.Client.Enumerations;
+
+namespace Speechmatics.Realtime.Microphone
+{
+    public class Program
+    {
+        private static readonly ProducerConsumerStream AudioStream = new ProducerConsumerStream();
+
+        private static string ToJson(object obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+
+        private static string RtUrl => "wss://192.168.128.30:9000/";
+
+        // ReSharper disable once UnusedParameter.Local
+        public static void Main(string[] args)
+        {
+            var t = new Task(() =>
+            {
+                var waveSource = new WaveInEvent {WaveFormat = new WaveFormat(44100, 16, 1)};
+                waveSource.DataAvailable += WaveSourceOnDataAvailable;
+                waveSource.StartRecording();
+            });
+            t.Start();
+
+            using (var stream = AudioStream)
+            {
+                try
+                {
+                    /*
+                     * The API constructor is passed the websockets URL, callbacks for the messages it might receive,
+                     * the language to transcribe (as a .NET CultureInfo object) and stream to read data from.
+                     */
+                    var config = new SmRtApiConfig("en", 44100, AudioFormatType.Raw, AudioFormatEncoding.PcmS16Le)
+                    {
+                        AddTranscriptCallback = Console.Write,
+                        AddPartialTranscriptMessageCallback = s => Console.Write("* " + s.transcript),
+                        ErrorMessageCallback = s => Console.WriteLine(ToJson(s)),
+                        WarningMessageCallback = s => Console.WriteLine(ToJson(s)),
+                        Insecure = true,
+                    };
+
+                    var api = new SmRtApi(RtUrl,
+                        stream,
+                        config
+                    );
+                    // Run() will block until the transcription is complete.
+                    Console.WriteLine($"Connecting to {RtUrl}");
+                    api.Run();
+                }
+                catch (AggregateException e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+
+            Console.WriteLine("End of stream");
+            Console.ReadLine();
+        }
+
+        private static void WaveSourceOnDataAvailable(object sender, WaveInEventArgs waveInEventArgs)
+        {
+            AudioStream.Write(waveInEventArgs.Buffer, 0, waveInEventArgs.BytesRecorded);
+        }
+    }
+}
