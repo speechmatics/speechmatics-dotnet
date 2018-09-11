ddfbb6e4f35dfc514e8fb6029004514e71cd3d83
James Gilmore <jamesg@speechmatics.com>
2018-09-11 10:42:27 +0100

    RTSP streaming example with ffmpeg

---
+++ RtspExample/RtspExample/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Newtonsoft.Json;
+using Speechmatics.Realtime.Client;
+
+namespace Speechmatics.Client.RtspExample
+{
+    public class Program
+    {
+        private static string ToJson(object obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+
+        private static string RtUrl => "wss://192.168.128.30:9000/";
+
+        // ReSharper disable once UnusedParameter.Local
+        public static void Main(string[] args)
+        {
+            var originalColor = Console.ForegroundColor;
+            var x = Console.CursorLeft;
+            var y = Console.CursorTop;
+
+            using (var rtsp = new RtspStream("rtsp://192.168.128.30:8554/test"))
+            {
+                try
+                {
+                    rtsp.Go();
+                    /*
+                     * The API constructor is passed the websockets URL, callbacks for the messages it might receive,
+                     * the language to transcribe (as a .NET CultureInfo object) and stream to read data from.
+                     */
+                    var config = new SmRtApiConfig("en")
+                    {
+                        AddTranscriptCallback = s =>
+                        {
+                            Console.SetCursorPosition(x, y);
+                            Console.Write(s);
+                            x = Console.CursorLeft;
+                            y = Console.CursorTop;
+                        },
+                        AddPartialTranscriptMessageCallback = s =>
+                        {
+                            Console.SetCursorPosition(x, y);
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            Console.Write(s.transcript);
+                            Console.ForegroundColor = originalColor;
+                        },
+                        ErrorMessageCallback = s => Console.WriteLine(ToJson(s)),
+                        WarningMessageCallback = s => Console.WriteLine(ToJson(s)),
+                        Insecure = true,
+                    };
+
+                    var api = new SmRtApi(RtUrl,
+                        rtsp,
+                        config
+                    );
+                    // Run() will block until the transcription is complete.
+                    Console.WriteLine($"Connecting to {RtUrl}");
+                    api.Run();
+                }
+                catch (AggregateException e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+
+            Console.WriteLine("End of stream");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RtspExample/RtspExample/RtspStream.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace Speechmatics.Client.RtspExample
+{
+    internal class RtspStream : Stream
+    {
+        private StreamReader _reader;
+        private readonly Process _ffmpegProcess;
+
+        public RtspStream(string rtspEndpoint)
+        {
+            var ffmpegPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "ffmpeg.exe");
+            var processStartInfo =
+                new ProcessStartInfo(ffmpegPath, $"-nostats -loglevel 1 -i {rtspEndpoint} -vn -f mp3 -")
+                {
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false
+                };
+            _ffmpegProcess = new Process { StartInfo = processStartInfo };
+        }
+
+        public void Go()
+        {
+            _ffmpegProcess.Start();
+            _reader = _ffmpegProcess.StandardOutput;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _ffmpegProcess?.Dispose();
+            base.Dispose();
+        }
+
+        public override void Flush()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            return _reader.BaseStream.Read(buffer, offset, count);
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool CanRead => true;
+        public override bool CanSeek => false;
+        public override bool CanWrite => false;
+        public override long Length { get; }
+
+        public override long Position
+        {
+            get => throw new NotSupportedException();
+            set => throw new NotSupportedException();
+        }
+    }
+}
